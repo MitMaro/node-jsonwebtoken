95ee5e7c5968345e653a9dd9b9dbb8fc6bc6515f
Tim Oram <dev@mitmaro.ca>
2018-06-15 08:40:41 -0230

    WIP

--- test/sign-validation.test.js
+++ test/sign-validation.test.js
@@ -8,69 +8,164 @@ describe('sign validation', function() {
   describe('for options', function() {
     [
       {
-        description: 'with expiresIn as a string with an invalid unit',
+        description: 'when not passed an object',
+        options: 'not an object',
+        expectedError: 'Expected "options" to be a plain object.'
+      },
+      {
+        description: 'when passed an unknown option',
+        options: {invalid: 'value'},
+        expectedError: '"invalid" is not allowed in "options"'
+      },
+      {
+        description: 'when passed the deprecated "expiresInSeconds" option',
+        options: {expiresInSeconds: 'value'},
+        expectedError: '"expiresInSeconds" is not allowed in "options"'
+      },
+      {
+        description: 'with "expiresIn" as a string with an invalid unit',
         options: {expiresIn: '1 monkey'},
         expectedError: '"expiresIn" should be a number of seconds or string representing a timespan'
       },
       {
-        description: 'with expiresIn as a float',
+        description: 'with "expiresIn" as a float',
         options: {expiresIn: 1.1},
         expectedError: '"expiresIn" should be a number of seconds or string representing a timespan'
       },
       {
-        description: 'with notBefore as a string with an invalid unit',
+        description: 'when "expiresIn" is provided with "exp" in payload',
+        options: {expiresIn: 100},
+        payload: {exp: 200},
+        expectedError: 'Bad "options.expiresIn" option the payload already has an "exp" property.'
+      },
+      {
+        description: 'with "notBefore" as a string with an invalid unit',
         options: {notBefore: '1 monkey'},
         expectedError: '"notBefore" should be a number of seconds or string representing a timespan'
       },
       {
-        description: 'with notBefore as a float',
+        description: 'with "notBefore" as a float',
         options: {notBefore: 1.1},
         expectedError: '"notBefore" should be a number of seconds or string representing a timespan'
       },
       {
-        description: 'with a non-string audience',
+        description: 'when "notBefore" is provided with "nbf" in payload',
+        options: {notBefore: 100},
+        payload: {nbf: 200},
+        expectedError: 'Bad "options.notBefore" option the payload already has an "nbf" property.'
+      },
+      {
+        description: 'with a non-string or non-array "audience"',
         options: {audience: 10},
         expectedError: '"audience" must be a string or array'
       },
       {
-        description: 'with a algorithm not in allowed list',
+        description: 'when "audience" is provided with "aud" in payload',
+        options: {audience: 'audience'},
+        payload: {aud: 'aud'},
+        expectedError: 'Bad "options.audience" option. The payload already has an "aud" property.'
+      },
+      {
+        description: 'with a "algorithm" not in allowed list',
         options: {algorithm: 'invalid'},
         expectedError: '"algorithm" must be a valid string enum value'
       },
       {
-        description: 'with a non-object header ',
+        description: 'with a non-object "header"',
         options: {header: 'invalid'},
         expectedError: '"header" must be an object'
       },
       {
-        description: 'with a non-string encoding',
+        description: 'with a non-string "encoding"',
         options: {encoding: 10},
         expectedError: '"encoding" must be a string'
       },
       {
-        description: 'with a non-string issuer',
+        description: 'with a non-string "issuer"',
         options: {issuer: 10},
         expectedError: '"issuer" must be a string'
       },
       {
-        description: 'with a non-string subject',
+        description: 'when "issuer" is provided with "iss" in payload',
+        options: {issuer: 'issuer'},
+        payload: {iss: 'iss'},
+        expectedError: 'Bad "options.issuer" option. The payload already has an "iss" property.'
+      },
+      {
+        description: 'with a non-string "subject"',
         options: {subject: 10},
         expectedError: '"subject" must be a string'
       },
       {
-        description: 'with a non-boolean noTimestamp',
+        description: 'when "subject" is provided with "sub" in payload',
+        options: {subject: 'subject'},
+        payload: {sub: 'sub'},
+        expectedError: 'Bad "options.subject" option. The payload already has an "sub" property.'
+      },
+      {
+        description: 'with a non-string "jwtid"',
+        options: {jwtid: 10},
+        expectedError: '"jwtid" must be a string'
+      },
+      {
+        description: 'when "jwtid" is provided with "jti" in payload',
+        options: {jwtid: 'jwtid'},
+        payload: {jti: 'jti'},
+        expectedError: 'Bad "options.jwtid" option. The payload already has an "jti" property.'
+      },
+      {
+        description: 'with a non-boolean "noTimestamp"',
         options: {noTimestamp: 'invalid'},
         expectedError: '"noTimestamp" must be a boolean'
       },
       {
-        description: 'with a non-string keyid',
+        description: 'with a non-string "keyid"',
         options: {keyid: 10},
         expectedError: '"keyid" must be a string'
+      },
+      {
+        description: 'with a non-string "keyid"',
+        options: {keyid: 10},
+        expectedError: '"keyid" must be a string'
+      },
+      {
+        description: 'with a non-string "mutatePayload"',
+        options: {mutatePayload: 'invalid'},
+        expectedError: '"mutatePayload" must be a boolean'
       }
     ].forEach((testCase) => {
       it(`should error ${testCase.description}`, function() {
-        expect(() => jwt.sign({}, 'secret', testCase.options)).to.throw(testCase.expectedError);
+        expect(() => jwt.sign(testCase.payload || {}, 'secret', testCase.options)).to.throw(testCase.expectedError);
       });
     });
-  })
+  });
+
+  describe('for payload', function() {
+    [
+      {
+        description: 'when provided an undefined payload',
+        payload: undefined,
+        expectedError: 'payload is required'
+      },
+      {
+        description: 'with a non-number "iat" claim',
+        payload: {iat: 'invalid'},
+        expectedError: '"iat" should be a number of seconds'
+      },
+      {
+        description: 'with a non-number "exp" claim',
+        payload: {exp: 'invalid'},
+        expectedError: '"exp" should be a number of seconds'
+      },
+      {
+        description: 'with a non-string "nbf" claim',
+        payload: {nbf: 'invalid'},
+        expectedError: '"nbf" should be a number of seconds'
+      },
+    ].forEach((testCase) => {
+      it(`should error ${testCase.description}`, function() {
+        expect(() => jwt.sign(testCase.payload, 'secret', {})).to.throw(testCase.expectedError);
+      });
+    });
+  });
 });
--- tests/schema.tests.js
+++ tests/schema.tests.js
@@ -15,39 +15,21 @@ describe('schema', function() {
     }
 
     it('should validate expiresIn', function () {
-      expect(function () {
-        sign({ expiresIn: '1 monkey' });
-      }).to.throw(/"expiresIn" should be a number of seconds or string representing a timespan/);
-      expect(function () {
-        sign({ expiresIn: 1.1 });
-      }).to.throw(/"expiresIn" should be a number of seconds or string representing a timespan/);
       sign({ expiresIn: '10s' });
       sign({ expiresIn: 10 });
     });
 
     it('should validate notBefore', function () {
-      expect(function () {
-        sign({ notBefore: '1 monkey' });
-      }).to.throw(/"notBefore" should be a number of seconds or string representing a timespan/);
-      expect(function () {
-        sign({ notBefore: 1.1 });
-      }).to.throw(/"notBefore" should be a number of seconds or string representing a timespan/);
       sign({ notBefore: '10s' });
       sign({ notBefore: 10 });
     });
 
     it('should validate audience', function () {
-      expect(function () {
-        sign({ audience: 10 });
-      }).to.throw(/"audience" must be a string or array/);
       sign({ audience: 'urn:foo' });
       sign({ audience: ['urn:foo'] });
     });
 
     it('should validate algorithm', function () {
-      expect(function () {
-        sign({ algorithm: 'foo' });
-      }).to.throw(/"algorithm" must be a valid string enum value/);
       sign({algorithm: 'RS256'});
       sign({algorithm: 'RS384'});
       sign({algorithm: 'RS512'});
@@ -61,74 +43,27 @@ describe('schema', function() {
     });
 
     it('should validate header', function () {
-      expect(function () {
-        sign({ header: 'foo' });
-      }).to.throw(/"header" must be an object/);
       sign({header: {}});
     });
 
     it('should validate encoding', function () {
-      expect(function () {
-        sign({ encoding: 10 });
-      }).to.throw(/"encoding" must be a string/);
       sign({encoding: 'utf8'});
     });
 
     it('should validate issuer', function () {
-      expect(function () {
-        sign({ issuer: 10 });
-      }).to.throw(/"issuer" must be a string/);
       sign({issuer: 'foo'});
     });
 
     it('should validate subject', function () {
-      expect(function () {
-        sign({ subject: 10 });
-      }).to.throw(/"subject" must be a string/);
       sign({subject: 'foo'});
     });
 
     it('should validate noTimestamp', function () {
-      expect(function () {
-        sign({ noTimestamp: 10 });
-      }).to.throw(/"noTimestamp" must be a boolean/);
       sign({noTimestamp: true});
     });
 
     it('should validate keyid', function () {
-      expect(function () {
-        sign({ keyid: 10 });
-      }).to.throw(/"keyid" must be a string/);
       sign({keyid: 'foo'});
-    });
-
-  });
-
-  describe('sign payload registered claims', function() {
-
-    function sign(payload) {
-      jwt.sign(payload, 'foo123');
-    }
-
-    it('should validate iat', function () {
-      expect(function () {
-        sign({ iat: '1 monkey' });
-      }).to.throw(/"iat" should be a number of seconds/);
-      sign({ iat: 10.1 });
-    });
-
-    it('should validate exp', function () {
-      expect(function () {
-        sign({ exp: '1 monkey' });
-      }).to.throw(/"exp" should be a number of seconds/);
-      sign({ exp: 10.1 });
-    });
-
-    it('should validate nbf', function () {
-      expect(function () {
-        sign({ nbf: '1 monkey' });
-      }).to.throw(/"nbf" should be a number of seconds/);
-      sign({ nbf: 10.1 });
     });
 
   });
